cbfdbb4bd61c202e06f4485c43967a67a1c1e3d8
Connor Toomey <cjt51@case.edu>
2014-09-06 18:31:49 -0400

    starting the app with back end files from old app and new things for the android

---
+++ User.java
@@ -0,0 +1,17 @@
+package learning.beer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Connor on 9/6/14.
+ */
+public class User {
+    private String username;
+    private ArrayList<Beer> userBeers;
+    private ArrayList<Flavors> userFlavors;
+
+
+
+
+
+}
